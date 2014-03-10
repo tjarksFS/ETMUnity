058545b9ba914ab4998cbf17c83e5d45db588884
Matthew Tjarks <tjarks@fullsail.edu>
2014-03-10 17:53:18 -0400

    Finished Roll-a-Ball tutorial

--- PutTheStuff/Assets/Scripts/PlayerController.cs
+++ PutTheStuff/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float speed;
+    public GUIText countText;
+    public GUIText winText;
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 
 	void FixedUpdate() 
 	{
@@ -20,7 +30,17 @@ public class PlayerController : MonoBehaviour
 		if(other.gameObject.tag == "PickUp")
 		{
 			other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
 		}
 	}
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
 }
