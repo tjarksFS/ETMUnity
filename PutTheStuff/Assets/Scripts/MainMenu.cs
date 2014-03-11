eae8aa8194b680bd55cc29224bd6bf29dfce2bbf
Matthew Tjarks <tjarks@fullsail.edu>
2014-03-11 16:37:06 -0400

    Added some textures and testing shooting projectiles

---
+++ PutTheStuff/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 50), "Start Game"))
+        {
+            Application.LoadLevel("Game");
+        }
+    }
+}
