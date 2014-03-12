07196d510a8ccd1e4311b1f3a7795e07ac2a05fd
Matthew Tjarks <tjarks@fullsail.edu>
2014-03-11 20:34:18 -0400

    Added moving target, started gyro testing

---
+++ PutTheStuff/Assets/Scripts/TargetController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetController : MonoBehaviour {
+
+    private bool left;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        left = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (left)
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            if (transform.position.x < -10)
+                left = false;
+        }
+        else
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            if (transform.position.x > 10)
+                left = true;
+        }
+	}
+}
